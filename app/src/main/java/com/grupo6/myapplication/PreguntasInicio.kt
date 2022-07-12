21529959d4faa04d2f2e424706a558c0371583a4
Marco Salazar <andressalazar714@gmail.com>
2022-07-11 23:09:19 -0500

    Navegación para actividad de agregar pregunta.

--- app/src/main/java/com/grupo6/myapplication/PreguntasInicio.kt
+++ app/src/main/java/com/grupo6/myapplication/PreguntasInicio.kt
@@ -1,10 +1,12 @@
 package com.grupo6.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,8 @@ class PreguntasInicio : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var bttnPregunta: Button
+    lateinit var vista: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +37,18 @@ class PreguntasInicio : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preguntas_inicio, container, false)
+        vista =  inflater.inflate(R.layout.fragment_preguntas_inicio, container, false)
+
+        bttnPregunta = vista.findViewById(R.id.bttnAgregar)
+
+        bttnPregunta.setOnClickListener(){
+            val intencion = Intent(getActivity(), ActivityAnadirPregunta::class.java)
+            startActivity(intencion)
+        }
+
+        return vista;
     }
 
     companion object {
--- app/src/main/java/com/grupo6/myapplication/VerPreguntaActivity.kt
+++ app/src/main/java/com/grupo6/myapplication/VerPreguntaActivity.kt
@@ -1,11 +1,18 @@
 package com.grupo6.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class VerPreguntaActivity : AppCompatActivity() {
+    lateinit var bttnPublicar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_pregunta)
+
+
+
+
     }
 }
