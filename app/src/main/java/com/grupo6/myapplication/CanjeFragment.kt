d5223ca816185f67424bc4859a4fb8d4cae860a4
JG <ludwing.gomez@epn.edu.ec>
2022-08-29 14:33:17 -0500

    Recuperar puntos del usuario
    Recuperacion de la base los puntos del usuario

--- app/src/main/java/com/grupo6/myapplication/CanjeFragment.kt
+++ app/src/main/java/com/grupo6/myapplication/CanjeFragment.kt
@@ -1,11 +1,22 @@
 package com.grupo6.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,14 +32,25 @@ class CanjeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var puntosDisponibles: TextView
+    lateinit var vista: View
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         getActivity()?.getWindow()?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         super.onCreate(savedInstanceState)
+
+
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
+
+
     }
 
     override fun onCreateView(
@@ -36,7 +58,11 @@ class CanjeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canje, container, false)
+        vista = inflater.inflate(R.layout.fragment_canje, container, false)
+        puntosDisponibles=vista.findViewById(R.id.textViewDescripcion)
+        consultarPuntosUsuario()
+
+        return vista
     }
 
     companion object {
@@ -57,5 +83,38 @@ class CanjeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    fun consultarPuntosUsuario(){
+        val database = Firebase.database.reference
+        val postListener = object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                if(dataSnapshot.exists() &&
+                    dataSnapshot.child("usuarios").exists()&&
+                    dataSnapshot.child("usuarios").childrenCount>0
+                ){
+                    var preguntas = ArrayList<Pregunta>()
+                    for (usuario  in dataSnapshot.child("usuarios").children){
+                        var usuarioObtenido = usuario.getValue<Usuario>() as Usuario
+                        println(usuarioObtenido.usuario)
+                        if(usuarioObtenido.usuario== usuarioLogeado){
+                            puntosDisponibles.setText(usuarioObtenido.puntos.toString())
+
+                            }
+
+                        }
+                    };
+
+
+                }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(EXTRA_LOGIN, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+
+        database.addValueEventListener(postListener)
     }
 }
